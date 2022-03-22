f3e5f5a61ff95ddb686ea8514eca673a047f7ade
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2022-03-22 09:44:03 +0700

    inisiasi project

---
+++ src/main.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var namalengkap: String = "Nugrah"
+    var alamat: String = "Kasongan"
+    var umur: Int = 18
+
+    println("Masukan Nama Anda : ")
+    var nama = readLine()
+
+    println("Nama Anda : $nama")
+    println(alamat)
+    println(umur)
+}
